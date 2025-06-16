036637faae48486eb7cedc2e84dee11cc62f16cb
ana granada <ana2002granada@gmail.com>
2025-06-15 19:49:04 -0500

    feat: add floating radio button component and translations

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Footer from './components/Footer.tsx';
 import { useTranslation } from 'react-i18next';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ROUTES } from './config/routes.tsx';
+import FloatingRadioButton from "./components/atoms/FloatingRadioButton";
 
 function App() {
   const { t, i18n } = useTranslation();
@@ -19,6 +20,7 @@ function App() {
       <div className="drawer">
         <input id="sidebar" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col">
+          <FloatingRadioButton/>
           <NavBar></NavBar>
           <Routes>
             {ROUTES.map(route => (
